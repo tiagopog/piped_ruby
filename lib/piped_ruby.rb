34000a6bf391c3797f974d36ae028eb052d4b913
Tiago Guedes <tiagopog@gmail.com>
2016-05-31 19:44:52 -0300

    :tada: Initial commit

---
+++ lib/piped_ruby.rb
@@ -0,0 +1,13 @@
+require 'piped_ruby/version'
+
+module PipedRuby
+  attr_accessor :piped
+
+  def |(&chained)
+    self.piped = call if piped.nil?
+    chained.piped = chained.call(piped)
+    chained
+  end
+end
+
+Proc.send :include, PipedRuby
---
+++ lib/piped_ruby/version.rb
@@ -0,0 +1,3 @@
+module PipedRuby
+  VERSION = "0.1.0"
+end
