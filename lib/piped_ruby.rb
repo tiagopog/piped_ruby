2e6464289705a0effe0e09b39fdedb7571058899
Tiago Guedes <tiagopog@gmail.com>
2016-06-01 18:53:32 -0300

    Add unwrap alias method

--- lib/piped_ruby.rb
+++ lib/piped_ruby.rb
@@ -3,6 +3,8 @@ require 'piped_ruby/version'
 module PipedRuby
   attr_accessor :piped
 
+  alias_method :unwrap, :piped
+
   def |(&chained)
     self.piped = call if piped.nil?
     chained.piped = chained.call(piped)
