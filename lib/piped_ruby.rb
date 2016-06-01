658762a5d308ccfb7ec8af86108927193981606a
Tiago Guedes <tiagopog@gmail.com>
2016-06-01 18:57:50 -0300

    Define a separate method to unwrap the piped content

--- lib/piped_ruby.rb
+++ lib/piped_ruby.rb
@@ -3,12 +3,14 @@ require 'piped_ruby/version'
 module PipedRuby
   attr_accessor :piped
 
-  alias_method :unwrap, :piped
-
   def |(&chained)
     self.piped = call if piped.nil?
     chained.piped = chained.call(piped)
     chained
+  end
+
+  def unwrap
+    piped || call
   end
 end
 
